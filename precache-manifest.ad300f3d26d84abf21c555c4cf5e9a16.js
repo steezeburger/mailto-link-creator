66f0df30dbb982cf58d53efd1b56e5f35b58fca1
Jesse Snyder <jessetsnyder@gmail.com>
2020-06-06 01:30:13 -0600

    Updates

---
+++ precache-manifest.ad300f3d26d84abf21c555c4cf5e9a16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370db96a09303f71cd583df9249d4ba5",
+    "url": "/mailto-link-creator/index.html"
+  },
+  {
+    "revision": "2ba7f89e1440997f9cce",
+    "url": "/mailto-link-creator/static/css/main.c9f9fdeb.chunk.css"
+  },
+  {
+    "revision": "7e5ebe6fa77afb20d54f",
+    "url": "/mailto-link-creator/static/js/2.c28f459a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mailto-link-creator/static/js/2.c28f459a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ba7f89e1440997f9cce",
+    "url": "/mailto-link-creator/static/js/main.fea7f953.chunk.js"
+  },
+  {
+    "revision": "e0f6a5d414e1a7002ba3",
+    "url": "/mailto-link-creator/static/js/runtime-main.44ec305a.js"
+  }
+]);
