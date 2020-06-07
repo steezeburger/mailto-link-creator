3414b70f97bbc434188c16b1203bae726daa9da4
jesse snyder <jessetsnyder@gmail.com>
2020-06-07 16:19:31 -0600

    deploy from local (#3)

--- app/bin/deploy.sh
+++ app/bin/deploy.sh
@@ -2,10 +2,10 @@
 
 branch=$(git rev-parse --abbrev-ref HEAD);
 
-eslint ..
+
 
 if [[ $branch == \"master\" ]]; then
-  yarn build && gh-pages -d build
+  eslint .. && yarn test && yarn build && gh-pages -d build
 else
   echo \"You can only deploy from master.\" && exit 1
 fi
